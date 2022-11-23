339bd4cfc2c803a419d6f13787e5484fdb05c3e0
arsnm <arsene.mallet2004@gmail.com>
2022-11-23 20:50:39 +0100

    upt gitignore

--- commit_changes.sh
+++ commit_changes.sh
@@ -4,7 +4,8 @@ cd `dirname "$0"`
 
 cp $HOME/.zshrc .
 cp -R $HOME/.zsh .
+cp $HOME/.config/starship.toml
 
 git add --all
-git commit -m "Sync zsh configs"
+git commit -m "Sync .dotfiles configs"
 git push
--- sync_zshrc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-DIR_PATH=`dirname "$0"`
-
-/opt/homebrew/bin/fswatch -o $HOME/.zshrc $HOME/.zsh/ | xargs -n1 zsh $DIR_PATH/commit_changes.sh
