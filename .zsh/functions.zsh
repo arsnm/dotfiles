9b470a01fc44f852c40ec7f6a254cc7dfee7dc1b
arsnm <arsene.mallet2004@gmail.com>
2023-02-10 00:36:01 +0100

    Sync .dotfiles configs

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -32,7 +32,7 @@ school() {
             if [ $? = 1 ]; then
                 git pull
             else
-                echo "No network, git pull aborted"
+                echo "No network, git pullaborted"
             fi
             if [ $# -ge 2 ]; then
                 echo 
