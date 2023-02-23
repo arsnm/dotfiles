04c31ffadfe2fa94910ca85a96d520900e05ac78
arsnm <arsene.mallet2004@gmail.com>
2023-02-23 14:23:53 +0100

    Sync dotfiles configs

--- sync_dotfiles.sh
+++ sync_dotfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
 DIR_PATH=`dirname "$0"`
-echo -n $DIR_PATH
+
 /opt/homebrew/bin/fswatch -o $HOME/.zshrc $HOME/.zsh/ $HOME/.config/starship.toml | xargs -n1 zsh $DIR_PATH/commit_changes.sh
