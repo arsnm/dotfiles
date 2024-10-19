4ebb4637119c9eb4018e83951cba1ffeecbcd315
arsnm <arsene.mallet2004@gmail.com>
2024-10-19 05:26:19 +0200

    updated vimtex configuration

--- nvim/lua/arsnm/plugins/vimtex.lua
+++ nvim/lua/arsnm/plugins/vimtex.lua
@@ -22,13 +22,9 @@ return {
         -- macos setup : use sioyek
         if os_name == "Darwin" then
             vim.g.vimtex_view_method = "sioyek"
-            vim.g.vimtex_view_sioyek_options =
-                "--reuse-window --forward-search-file @pdf --forward-search-line @line @tex"
         -- linux setup : use zathura
         elseif os_name == "Linux" then
             vim.g.vimtex_view_method = "zathura"
-            vim.g.vimtex_view_zathura_options =
-                '--synctex-forward @line:0:@tex --synctex-editor-command "nvim --headless +%l %f"'
         end
 
         vim.g.vimtex_quickfix_open_on_warning = 0
