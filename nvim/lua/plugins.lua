5f13725eac95040921ca54d535e9d869dd5407bc
arsnm <arsene.mallet2004@gmail.com>
2026-03-19 13:38:14 +0100

    Started writing my linux install guide
    Signed-off-by: arsnm <arsene.mallet2004@gmail.com>

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,6 +3,23 @@
 -- are required manually).
 -- The plugins are automatically updadted very Monday.
 
+-- Plugins list
+vim.pack.add({
+    { src = "https://github.com/blazkowolf/gruber-darker.nvim" },
+    { src = "https://github.com/echasnovski/mini.pick" },
+    { src = "https://github.com/echasnovski/mini.icons" },
+    { src = "https://github.com/m3xshen/autoclose.nvim" },
+    { src = "https://github.com/stevearc/oil.nvim" },
+    { src = "https://github.com/mason-org/mason.nvim" },
+    { src = "https://github.com/saghen/blink.cmp",             version = vim.version.range("*") },
+    { src = "https://github.com/stevearc/conform.nvim" },
+    { src = "https://github.com/L2MON4D3/LuaSnip" },
+    { src = "https://github.com/rafamadriz/friendly-snippets" },
+    { src = "https://github.com/catgoose/nvim-colorizer.lua" },
+    { src = "https://github.com/zbirenbaum/copilot.lua" },
+    { src = "https://github.com/neovim/nvim-lspconfig" },
+})
+
 -- Auto-update plugins
 local update_file = vim.fn.stdpath("config") .. "/last_update.txt"
 local last_update = nil
@@ -23,23 +40,6 @@ if weekday_num == '1' and last_update ~= today then
         f_w:close()
     end
 end
-
--- Plugins list
-vim.pack.add({
-    { src = "https://github.com/blazkowolf/gruber-darker.nvim" },
-    { src = "https://github.com/echasnovski/mini.pick" },
-    { src = "https://github.com/echasnovski/mini.icons" },
-    { src = "https://github.com/m4xshen/autoclose.nvim" },
-    { src = "https://github.com/stevearc/oil.nvim" },
-    { src = "https://github.com/mason-org/mason.nvim" },
-    { src = "https://github.com/saghen/blink.cmp",             version = vim.version.range("*") },
-    { src = "https://github.com/stevearc/conform.nvim" },
-    { src = "https://github.com/L3MON4D3/LuaSnip" },
-    { src = "https://github.com/rafamadriz/friendly-snippets" },
-    { src = "https://github.com/catgoose/nvim-colorizer.lua" },
-    { src = "https://github.com/zbirenbaum/copilot.lua" },
-    { src = "https://github.com/neovim/nvim-lspconfig" },
-})
 
 -- Load plugins configuration
 require("plugins_conf.mini_pick")
