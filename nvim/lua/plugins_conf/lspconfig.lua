2a9805acec7c8533a4dab985ecb678981e4ae27f
arsnm <arsene.mallet2004@gmail.com>
2026-06-24 12:23:40 +0200

    nvim: tinymist lsp + lsp keymap
    Signed-off-by: arsnm <arsene.mallet2004@gmail.com>

--- nvim/lua/plugins_conf/lspconfig.lua
+++ nvim/lua/plugins_conf/lspconfig.lua
@@ -1,1 +1,9 @@
-vim.lsp.enable({ 'lua_ls', 'pyright', 'clangd', 'bashls', 'jdtls', 'rust_analyzer'})
+vim.lsp.enable({ 'lua_ls', 'pyright', 'clangd', 'bashls', 'jdtls', 'rust_analyzer', 'tinymist' })
+
+
+-- Close quickfix/location list command
+vim.keymap.set('n', 'grq', '<cmd>silent! cclose<CR> | silent! lclose<CR>', {
+    noremap = true,
+    silent = true,
+    desc = "Close quickfix/location list"
+})
